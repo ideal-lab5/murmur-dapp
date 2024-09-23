acd851cd75f9a483bdf6eb1f5d75f9c036dae1e8
Juan Girini <juangirini@gmail.com>
2024-09-23 09:53:26 +0200

    resolve conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,31 @@
 'use client'
 
+// import { container } from '@/app/di/containerConfig'
+import { Button } from '@/components/button'
+import EtfButton from '@/components/etf/etfButton'
 import { Heading, Subheading } from '@/components/heading'
-import { murmurClient, ipfsClient } from './murmurClient';
-import "reflect-metadata";
-import { Button } from '@/components/button';
+import 'reflect-metadata'
+import { ipfsClient, murmurClient } from './murmurClient'
 
 export default function Home() {
-
-  console.log(`Service instances murmur: ${murmurClient} and ipfs ${ipfsClient}`);
+  console.log(`Service instances murmur: ${murmurClient} and ipfs ${ipfsClient}`)
 
   async function handleIpfsTest() {
-    const cid = await ipfsClient.writeObject({ hello: 'world' });
-    console.log(`CID: ${cid}`);
-    const object = await ipfsClient.getObject(cid);
-    console.log(`Object: ${object}`);
+    const cid = await ipfsClient.writeObject({ hello: 'world' })
+    console.log(`CID: ${cid}`)
+    const object = await ipfsClient.getObject(cid)
+    console.log(`Object: ${object}`)
   }
 
   return (
     <>
       <Heading>Murmur Wallet</Heading>
       <Subheading>Coming soon! Work in progress...</Subheading>
-      <Button color='purple' onClick={() => handleIpfsTest()}> IPFS test</Button>
+      <Button color="purple" onClick={() => handleIpfsTest()}>
+        {' '}
+        IPFS test
+      </Button>
+      <EtfButton />
     </>
   )
 }
