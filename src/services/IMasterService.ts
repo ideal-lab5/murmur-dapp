8be52ef5eab56f5cfae9a3a91b168977efc2f0da
Juan Girini <juangirini@gmail.com>
2024-10-16 21:19:37 +0200

    test: styling

--- src/services/IMasterService.ts
+++ src/services/IMasterService.ts
@@ -1,5 +1,5 @@
 import type { KeyringPair } from '@polkadot/keyring/types'
 
 export interface IMasterService {
-  getMasterAccount: () =>    KeyringPair
+  getMasterAccount: () =>      KeyringPair
 }
